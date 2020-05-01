9c865d9a4411c7c20e644b56ee8999e240b4ce66
Ian <ian.goofygoober@gmail.com>
2020-05-01 16:17:16 -0500

    Update WALLE_jump.lua

--- WALLE_jump.lua
+++ WALLE_jump.lua
@@ -1,4 +1,4 @@
-print("WALL-E jump lua script v1.1")
+print("WALL-E jump lua script")
 print("created by Schnert0")
 print("---")
 print("jump with the select button")
@@ -13,6 +13,7 @@ state = nil
 driveX, driveY = nil, nil
 
 canJump = true
+cantJump = false
 
 while(true) do
 	-- gets the addresses of all the memory variables
@@ -24,6 +25,8 @@ while(true) do
 
 	state = memory.readdword(0x021D4CB4) + 0x54
 	onSlope = memory.readdword(0x21D4CB4) + 0xCC
+	slopeTestX = memory.readdword(0x21D4CB4) + 0x90
+	slopeTestY = memory.readdword(0x21D4CB4) + 0x94
 	driveX = memory.readdword(0x021D4CB8) + 0x50
 	driveY = memory.readdword(0x021D4CB8) + 0x54
 	
@@ -31,18 +34,35 @@ while(true) do
 	prevInput = input
 	input = joypad.get()
 	
+	slope = memory.readbyte(onSlope)
+	testX = memory.readword(slopeTestX)
+	testY = memory.readword(slopeTestY)
+	
+	dX = memory.readword(driveX)
+	dY = memory.readword(driveY)
+	
 	if(input.select and not prevInput.select)then -- has the player pressed the jump button?
-		slope = memory.readbyte(onSlope)
-		if((memory.readdword(zVel) == 0 and slope == 1) or slope == 2)then -- is the on flat ground or on a slope?
+		if((memory.readdword(zVel) == 0 and slope == 1) or slope == 2)then
 			canJump = true
 		end
 		
-		if(canJump == true)then
+		
+		
+		if(cantJump and memory.readdword(zVel) == 0)then
+			cantJump = false
+		end
+		
+		
+		if(canJump and not cantJump)then
 			canJump = false
 			
 			-- turns s16 int s32
 			x = memory.readword(driveX)*2
 			y = memory.readword(driveY)*2
+			
+			if(slope > 1 and testX ==0 and testY == 0)then
+				cantJump = true
+			end
 			
 			if(bit.band(x, 0x10000) == 0x10000) then
 				x = bit.bor(x, 0xfffe0000)
